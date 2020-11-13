5d4ff2b240244aebb0321ee0c040564aae2f76b5
Nirmalraj10567 <55886074+Nirmalraj10567@users.noreply.github.com>
2020-11-13 14:02:40 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -4,6 +4,29 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const TeleBot = require('telebot');
+
+const bot = new TeleBot('1238769131:AAGXzi8rLKi5dt9ejNMRE4iV-9aqT0e18eo');
+
+bot.on(/^\/say (.+)$/, async(msg, props) => {
+
+    const text =await props.match[1];
+
+    const axios = require('axios')
+
+const zx = axios.get('https://gplinks.in/api?api=1b1093a7f93afbe94ad2df18b89861d4970c23b8&url='+text)
+
+  .then((response) => {
+
+  A= response.data.shortenedUrl
+
+    return bot.sendMessage(msg.from.id, A,{ replyToMessage: msg.message_id });
+
+}) 
+
+});
+
+bot.start()
 
 const router = express.Router();
 router.get('/', (req, res) => {
